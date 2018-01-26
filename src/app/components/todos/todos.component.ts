9b03f691b95e5251b814d8b7f0d48738036981f0
marcharriss <marcwilliamh@hotmail.com>
2018-01-26 17:17:54 +0100

    finish first todo page

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todoList:string[];
+  todoInput:string;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.todoList = ['You have nothing to-do yet'];
+
+  }
+
+  addTodo(todo) {
+    this.todoList.push(todo);
+    console.log(todo);
+    console.log(this.todoList);
+    this.todoInput = '';
+  }
+
+  deleteTodo(i) {
+    this.todoList.splice(i, 1);
   }
 
 }
