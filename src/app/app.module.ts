4e0ddc77539e5fe9969417cd341aa1d5bab14a07
marcharriss <marcwilliamh@hotmail.com>
2018-01-27 08:26:22 +0100

    add profile files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavagationComponent } from './components/navagation/navagation.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
+const appRoutes:Routes = [
+  {path:"" , component:UsersComponent},
+  {path:"profile", component:ProfileComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     UsersComponent,
-    NavagationComponent
+    NavagationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
